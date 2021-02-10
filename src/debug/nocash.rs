6fb500a6e8540a17023b2f04240c3a1cf04bc5d3
Alissa Rao <lymia@lymiahugs.com>
2021-02-10 05:40:23 -0800

    Implement a debugging interface that allows the use of debugging on multiple emulators.

---
+++ src/debug/nocash.rs
@@ -0,0 +1,6 @@
+//! Special utils for if you're running on the NO$GBA emulator.
+//!
+//! Note that this assumes that you're using the very latest version (3.03). If
+//! you've got some older version of things there might be any number of
+//! differences or problems.
+
--- src/macros.rs
+++ src/macros.rs
@@ -87,97 +87,84 @@ macro_rules! newtype_enum {
   };
 }
 
-/// Delivers a fatal message to the mGBA output, halting emulation.
+/// Delivers a fatal message to the emulator debug output, and crashes
+/// the the game.
 ///
-/// This works basically like `println`. mGBA is a C program and all, so you
-/// should only attempt to print non-null ASCII values through this. There's
-/// also a maximum length of 255 bytes per message.
+/// This works basically like `println`. You should avoid null ASCII values.
+/// Furthermore on mGBA, there is a maximum length of 255 bytes per message.
 ///
-/// This has no effect if you're not using mGBA.
+/// This has no effect outside of a supported emulator.
 #[macro_export]
 macro_rules! fatal {
   ($($arg:tt)*) => {{
-    use $crate::mgba::{MGBADebug, MGBADebugLevel};
-    use core::fmt::Write;
-    if let Some(mut mgba) = MGBADebug::new() {
-      let _ = write!(mgba, $($arg)*);
-      mgba.send(MGBADebugLevel::Fatal);
+    use $crate::debug;
+    if !debug::is_debugging_disabled() {
+      debug::debug_print(debug::DebugLevel::Fatal, &format_args!($($arg)*)).ok();
     }
+    debug::crash()
   }};
 }
 
-/// Delivers an error message to the mGBA output.
+/// Delivers a error message to the emulator debug output.
 ///
-/// This works basically like `println`. mGBA is a C program and all, so you
-/// should only attempt to print non-null ASCII values through this. There's
-/// also a maximum length of 255 bytes per message.
+/// This works basically like `println`. You should avoid null ASCII values.
+/// Furthermore on mGBA, there is a maximum length of 255 bytes per message.
 ///
-/// This has no effect if you're not using mGBA.
+/// This has no effect outside of a supported emulator.
 #[macro_export]
 macro_rules! error {
   ($($arg:tt)*) => {{
-    use $crate::mgba::{MGBADebug, MGBADebugLevel};
-    use core::fmt::Write;
-    if let Some(mut mgba) = MGBADebug::new() {
-      let _ = write!(mgba, $($arg)*);
-      mgba.send(MGBADebugLevel::Error);
+    use $crate::debug;
+    if !debug::is_debugging_disabled() {
+      debug::debug_print(debug::DebugLevel::Error, &format_args!($($arg)*)).ok();
     }
   }};
 }
 
-/// Delivers a warning message to the mGBA output.
+/// Delivers a warning message to the emulator debug output.
 ///
-/// This works basically like `println`. mGBA is a C program and all, so you
-/// should only attempt to print non-null ASCII values through this. There's
-/// also a maximum length of 255 bytes per message.
+/// This works basically like `println`. You should avoid null ASCII values.
+/// Furthermore on mGBA, there is a maximum length of 255 bytes per message.
 ///
-/// This has no effect if you're not using mGBA.
+/// This has no effect outside of a supported emulator.
 #[macro_export]
 macro_rules! warn {
   ($($arg:tt)*) => {{
-    use $crate::mgba::{MGBADebug, MGBADebugLevel};
-    use core::fmt::Write;
-    if let Some(mut mgba) = MGBADebug::new() {
-      let _ = write!(mgba, $($arg)*);
-      mgba.send(MGBADebugLevel::Warning);
+    use $crate::debug;
+    if !debug::is_debugging_disabled() {
+      debug::debug_print(debug::DebugLevel::Warning, &format_args!($($arg)*)).ok();
     }
   }};
 }
 
-/// Delivers an info message to the mGBA output.
+/// Delivers an info message to the emulator debug output.
 ///
-/// This works basically like `println`. mGBA is a C program and all, so you
-/// should only attempt to print non-null ASCII values through this. There's
-/// also a maximum length of 255 bytes per message.
+/// This works basically like `println`. You should avoid null ASCII values.
+/// Furthermore on mGBA, there is a maximum length of 255 bytes per message.
 ///
-/// This has no effect if you're not using mGBA.
+/// This has no effect outside of a supported emulator.
 #[macro_export]
 macro_rules! info {
   ($($arg:tt)*) => {{
-    use $crate::mgba::{MGBADebug, MGBADebugLevel};
-    use core::fmt::Write;
-    if let Some(mut mgba) = MGBADebug::new() {
-      let _ = write!(mgba, $($arg)*);
-      mgba.send(MGBADebugLevel::Info);
+    use $crate::debug;
+    if !debug::is_debugging_disabled() {
+      debug::debug_print(debug::DebugLevel::Info, &format_args!($($arg)*)).ok();
     }
   }};
 }
 
-/// Delivers a debug message to the mGBA output.
+/// Delivers a debug message to the emulator debug output.
 ///
-/// This works basically like `println`. mGBA is a C program and all, so you
-/// should only attempt to print non-null ASCII values through this. There's
-/// also a maximum length of 255 bytes per message.
+/// This works basically like `println`. You should avoid null ASCII values.
+/// Furthermore on mGBA, there is a maximum length of 255 bytes per message.
 ///
-/// This has no effect if you're not using mGBA.
+/// This has no effect outside of a supported emulator.
 #[macro_export]
 macro_rules! debug {
   ($($arg:tt)*) => {{
-    use $crate::mgba::{MGBADebug, MGBADebugLevel};
-    use core::fmt::Write;
-    if let Some(mut mgba) = MGBADebug::new() {
-      let _ = write!(mgba, $($arg)*);
-      mgba.send(MGBADebugLevel::Debug);
+    use $crate::debug;
+    if !debug::is_debugging_disabled() {
+      debug::debug_print(debug::DebugLevel::Debug, &format_args!($($arg)*)).ok();
     }
   }};
 }
