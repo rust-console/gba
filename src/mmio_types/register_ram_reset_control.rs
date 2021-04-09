a67fd3881423530e7d2027975209b6c461d63011
Lokathor <zefria@gmail.com>
2021-04-08 18:28:30 -0600

    block resetting work ram for now.

--- src/mmio_types/register_ram_reset_control.rs
+++ src/mmio_types/register_ram_reset_control.rs
@@ -5,8 +5,8 @@ use super::*;
 pub struct ResetFlags(pub(crate) u8);
 impl ResetFlags {
   const_new!();
-  bitfield_bool!(u8; 0, ewram, with_ewram, set_ewram);
-  bitfield_bool!(u8; 1, iwram, with_iwram, set_iwram);
+  //bitfield_bool!(u8; 0, ewram, with_ewram, set_ewram);
+  //bitfield_bool!(u8; 1, iwram, with_iwram, set_iwram);
   bitfield_bool!(u8; 2, palram, with_palram, set_palram);
   bitfield_bool!(u8; 3, vram, with_vram, set_vram);
   bitfield_bool!(u8; 4, oam, with_oam, set_oam);
