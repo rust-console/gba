331c672b25545bb72682653060d40fbb3c627902
Mason Ginter <mason@dagint.com>
2021-05-04 18:56:29 -0400

    switch macro_export to pub(crate)

--- src/random/algorithms.rs
+++ src/random/algorithms.rs
@@ -1,29 +1,30 @@
 
 /// Advances a PCG with 32 bits of state.
-#[macro_export]
 macro_rules! pcg_core_state32 {
     ($state:expr, $inc:expr) => {
         $state.wrapping_mul(PCG_MULTIPLIER_32).wrapping_add($inc)
     };
 }
-#[macro_export]
-macro_rules! rxs_m_xs_u32_to_u32 {
+pub(crate) use pcg_core_state32;
+
+macro_rules! pcg_rxs_m_xs_u32_to_u32 {
     ($state: expr) => {{
         $state ^= ($state >> (4 + ($state >> 28) as u32)).wrapping_mul(277803737u32);
         $state ^ ($state >> 22)
     }};
 }
-#[macro_export]
+pub(crate) use pcg_rxs_m_xs_u32_to_u32;
+
 macro_rules! pcg_xsh_rr_u32_to_u16 {
     ($state: expr) => {
         ((($state ^ ($state >> 18)) >> 11) as u16).rotate_right($state >> 27) as u16 
     };
 }
-/// 29 = 32 - 3 (determine shift amount)
-/// 6 = 16 - 3 - 7 ()
-#[macro_export]
+pub(crate) use pcg_xsh_rr_u32_to_u16;
+
 macro_rules! pcg_xsh_rs_u32_to_u16 {
     ($state: expr) => {
         (($state ^ ($state >> 6)) >> (6 + ($state >> 29))) as u16 
     };
 }
+pub(crate) use pcg_xsh_rs_u32_to_u16;
