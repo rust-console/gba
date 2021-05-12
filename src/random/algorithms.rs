ef7039e39c32bc00ca1a83b2ca6dc19c12ae8115
Mason Ginter <mason@dagint.com>
2021-05-12 16:26:33 -0400

    More commenting

--- src/random/algorithms.rs
+++ src/random/algorithms.rs
@@ -5,7 +5,7 @@ macro_rules! pcg_core_state32 {
   };
 }
 pub(crate) use pcg_core_state32;
-
+/// Generates u32 from u32 state
 macro_rules! pcg_rxs_m_xs_u32_to_u32 {
   ($state: expr) => {{
     $state ^= ($state >> (4 + ($state >> 28) as u32)).wrapping_mul(277803737u32);
@@ -21,7 +21,7 @@ pub(crate) use pcg_rxs_m_xs_u32_to_u32;
 //     };
 // }
 // pub(crate) use pcg_xsh_rr_u32_to_u16;
-
+/// Generates u16 from u32 state
 macro_rules! pcg_xsh_rs_u32_to_u16 {
   ($state: expr) => {
     (($state ^ ($state >> 6)) >> (6 + ($state >> 29))) as u16
