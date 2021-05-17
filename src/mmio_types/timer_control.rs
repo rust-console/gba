093edc703f76ef4762f40b72eb00e6f96badae05
Alissa Rao <lymia@lymiahugs.com>
2021-05-17 10:50:42 -0700

    Bitfield fixes (#146)
    * Fix incorrect bit for `chained_counting` field in TimerControl.
    
    * Fixes incorrect bitfield manipulation math in mmio_types.

--- src/mmio_types/timer_control.rs
+++ src/mmio_types/timer_control.rs
@@ -6,7 +6,7 @@ pub struct TimerControl(u8);
 impl TimerControl {
   const_new!();
   bitfield_int!(u8; 0..=1: u8, prescaler_selection, with_prescaler_selection, set_prescaler_selection);
-  bitfield_bool!(u8; 4, chained_counting, with_chained_counting, set_chained_counting);
+  bitfield_bool!(u8; 2, chained_counting, with_chained_counting, set_chained_counting);
   bitfield_bool!(u8; 6, irq_on_overflow, with_irq_on_overflow, set_irq_on_overflow);
   bitfield_bool!(u8; 7, enabled, with_enabled, set_enabled);
 }
