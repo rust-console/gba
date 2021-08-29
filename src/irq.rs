b84788a310a5d6dcbb8cb8c884f9474165640cd9
Ruin0x11 <ipickering2@gmail.com>
2021-08-29 06:41:36 -0700

    Rename

--- src/irq.rs
+++ src/irq.rs
@@ -83,43 +83,43 @@ pub enum IrqKind {
 
 /// Offset from I/O address space to relevant MMIO address and the bitmask to
 /// set/unset to enable/disable an interrupt.
-struct IrqOffsets {
-    base: usize,
+struct IrqSender {
+    offset: usize,
     mask: u16
 }
 
-static OFFSETS: [IrqOffsets; 14] = [
-    IrqOffsets { base: 0x0004, mask: 0x0008 }, // VBlank  DISPSTAT
-    IrqOffsets { base: 0x0004, mask: 0x0010 }, // HBlank  DISPSTAT
-    IrqOffsets { base: 0x0004, mask: 0x0020 }, // VCount  DISPSTAT
-    IrqOffsets { base: 0x0102, mask: 0x0040 }, // Timer0  TM0CNT
-    IrqOffsets { base: 0x0106, mask: 0x0040 }, // Timer1  TM1CNT
-    IrqOffsets { base: 0x010A, mask: 0x0040 }, // Timer2  TM2CNT
-    IrqOffsets { base: 0x010E, mask: 0x0040 }, // Timer3  TM3CNT
-    IrqOffsets { base: 0x0128, mask: 0x4000 }, // Serial  SIOCNT
-    IrqOffsets { base: 0x00BA, mask: 0x4000 }, // DMA0    DMA0CNT
-    IrqOffsets { base: 0x00C6, mask: 0x4000 }, // DMA1    DMA1CNT
-    IrqOffsets { base: 0x00D2, mask: 0x4000 }, // DMA2    DMA2CNT
-    IrqOffsets { base: 0x00DE, mask: 0x4000 }, // DMA3    DMA3CNT
-    IrqOffsets { base: 0x0132, mask: 0x4000 }, // Keypad  KEYCNT
-    IrqOffsets { base: 0x0000, mask: 0x0000 }, // Gamepak (none)
+static SENDERS: [IrqSender; 14] = [
+    IrqSender { offset: 0x0004, mask: 0x0008 }, // VBlank  DISPSTAT
+    IrqSender { offset: 0x0004, mask: 0x0010 }, // HBlank  DISPSTAT
+    IrqSender { offset: 0x0004, mask: 0x0020 }, // VCount  DISPSTAT
+    IrqSender { offset: 0x0102, mask: 0x0040 }, // Timer0  TM0CNT
+    IrqSender { offset: 0x0106, mask: 0x0040 }, // Timer1  TM1CNT
+    IrqSender { offset: 0x010A, mask: 0x0040 }, // Timer2  TM2CNT
+    IrqSender { offset: 0x010E, mask: 0x0040 }, // Timer3  TM3CNT
+    IrqSender { offset: 0x0128, mask: 0x4000 }, // Serial  SIOCNT
+    IrqSender { offset: 0x00BA, mask: 0x4000 }, // DMA0    DMA0CNT
+    IrqSender { offset: 0x00C6, mask: 0x4000 }, // DMA1    DMA1CNT
+    IrqSender { offset: 0x00D2, mask: 0x4000 }, // DMA2    DMA2CNT
+    IrqSender { offset: 0x00DE, mask: 0x4000 }, // DMA3    DMA3CNT
+    IrqSender { offset: 0x0132, mask: 0x4000 }, // Keypad  KEYCNT
+    IrqSender { offset: 0x0000, mask: 0x0000 }, // Gamepak (none)
 ];
 
 const MMIO_BASE_ADDRESS: usize = 0x0400_0000;
 
 #[inline]
 unsafe fn set_irq_flag(kind: IrqKind) {
-    let offset = &OFFSETS[kind as usize];
-    let addr: *mut u16 = core::mem::transmute(MMIO_BASE_ADDRESS + offset.base);
-    *addr |= offset.mask;
+    let sender = &SENDERS[kind as usize];
+    let addr: *mut u16 = core::mem::transmute(MMIO_BASE_ADDRESS + sender.offset);
+    *addr |= sender.mask;
     IE.write(InterruptFlags(IE.read().0 | (1 << (kind as u8))))
 }
 
 #[inline]
 unsafe fn unset_irq_flag(kind: IrqKind) {
-    let offset = &OFFSETS[kind as usize];
-    let addr: *mut u16 = core::mem::transmute(MMIO_BASE_ADDRESS + offset.base);
-    *addr &= !offset.mask;
+    let sender = &SENDERS[kind as usize];
+    let addr: *mut u16 = core::mem::transmute(MMIO_BASE_ADDRESS + sender.offset);
+    *addr &= !sender.mask;
     IE.write(InterruptFlags(IE.read().0 & !(1 << (kind as u8))))
 }
 
@@ -145,9 +145,7 @@ pub fn set_master_enabled(enabled: bool) {
 /// Clears all registered interrupt handlers.
 pub fn clear_all_handlers() {
     with_no_irqs! {
-        unsafe {
-            __IRQ_HANDLERS = [None; 14];
-        }
+        unsafe { __IRQ_HANDLERS = [None; 14]; }
     }
 }
 
@@ -158,24 +156,18 @@ pub fn clear_all_handlers() {
 /// with `#[instruction_set(arm::t32)]`.
 pub fn set_handler(kind: IrqKind, handler: Option<IrqHandler>) {
     with_no_irqs! {
-        unsafe {
-            __IRQ_HANDLERS[kind as usize] = handler;
-        }
+        unsafe { __IRQ_HANDLERS[kind as usize] = handler; }
     }
 }
 
 pub fn enable(kind: IrqKind) {
     with_no_irqs! {
-        unsafe {
-            set_irq_flag(kind);
-        }
+        unsafe { set_irq_flag(kind); }
     }
 }
 
 pub fn disable(kind: IrqKind) {
     with_no_irqs! {
-        unsafe {
-            unset_irq_flag(kind);
-        }
+        unsafe { unset_irq_flag(kind); }
     }
 }
