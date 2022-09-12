4fd2b1bb940a5331c32da3a89192a56c749bb875
Lokathor <zefria@gmail.com>
2022-09-12 11:38:36 -0600

    finish video MMIO basics, no docs yet.

--- src/mmio.rs
+++ src/mmio.rs
@@ -17,11 +17,14 @@ macro_rules! def_mmio {
 }
 
 use core::ffi::c_void;
+use bitfrob::u8x2;
 use voladdress::{Safe, Unsafe, VolAddress, VolBlock};
 use crate::{
   interrupts::IrqBits,
-  video::{BackgroundControl, Color, DisplayControl, DisplayStatus},
+  video::{BackgroundControl, Color, DisplayControl, DisplayStatus, WindowInside, WindowOutside, Mosaic, BlendControl},
 };
+
+// Video
 
 def_mmio!(0x0400_0000 = DISPCNT: VolAddress<DisplayControl, Safe, Safe>; "Display Control");
 def_mmio!(0x0400_0004 = DISPSTAT: VolAddress<DisplayStatus, Safe, Safe>; "Display Status");
@@ -30,36 +33,38 @@ def_mmio!(0x0400_0008 = BG0CNT: VolAddress<BackgroundControl, Safe, Safe>; "Back
 def_mmio!(0x0400_000A = BG1CNT: VolAddress<BackgroundControl, Safe, Safe>; "Background 1 Control");
 def_mmio!(0x0400_000C = BG2CNT: VolAddress<BackgroundControl, Safe, Safe>; "Background 2 Control");
 def_mmio!(0x0400_000E = BG3CNT: VolAddress<BackgroundControl, Safe, Safe>; "Background 3 Control");
-def_mmio!(0x0400_0010 = BG0HOFS: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0012 = BG0VOFS: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0014 = BG1HOFS: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0016 = BG1VOFS: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0018 = BG2HOFS: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_001A = BG2VOFS: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_001C = BG3HOFS: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_001E = BG3VOFS: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0020 = BG2PA: VolAddress<i16, (), Safe>);
-def_mmio!(0x0400_0022 = BG2PB: VolAddress<i16, (), Safe>);
-def_mmio!(0x0400_0024 = BG2PC: VolAddress<i16, (), Safe>);
-def_mmio!(0x0400_0026 = BG2PD: VolAddress<i16, (), Safe>);
-def_mmio!(0x0400_0028 = BG2X: VolAddress<i32, (), Safe>);
-def_mmio!(0x0400_002C = BG2Y: VolAddress<i32, (), Safe>);
-def_mmio!(0x0400_0030 = BG3PA: VolAddress<i16, (), Safe>);
-def_mmio!(0x0400_0032 = BG3PB: VolAddress<i16, (), Safe>);
-def_mmio!(0x0400_0034 = BG3PC: VolAddress<i16, (), Safe>);
-def_mmio!(0x0400_0036 = BG3PD: VolAddress<i16, (), Safe>);
-def_mmio!(0x0400_0038 = BG3X: VolAddress<i32, (), Safe>);
-def_mmio!(0x0400_003C = BG3Y: VolAddress<i32, (), Safe>);
-def_mmio!(0x0400_0040 = WIN0H: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0042 = WIN1H: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0044 = WIN0V: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0046 = WIN1V: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0048 = WININ: VolAddress<u16, Safe, Safe>);
-def_mmio!(0x0400_004A = WINOUT: VolAddress<u16, Safe, Safe>);
-def_mmio!(0x0400_004C = MOSAIC: VolAddress<u16, (), Safe>);
-def_mmio!(0x0400_0050 = BLDCNT: VolAddress<u16, Safe, Safe>);
-def_mmio!(0x0400_0052 = BLDALPHA: VolAddress<u16, Safe, Safe>);
-def_mmio!(0x0400_0054 = BLDY: VolAddress<u16, (), Safe>);
+def_mmio!(0x0400_0010 = BG0HOFS: VolAddress<u16, (), Safe>; "Background 0 Horizontal Offset (text mode)");
+def_mmio!(0x0400_0012 = BG0VOFS: VolAddress<u16, (), Safe>; "Background 0 Vertical Offset (text mode)");
+def_mmio!(0x0400_0014 = BG1HOFS: VolAddress<u16, (), Safe>; "Background 1 Horizontal Offset (text mode)");
+def_mmio!(0x0400_0016 = BG1VOFS: VolAddress<u16, (), Safe>; "Background 1 Vertical Offset (text mode)");
+def_mmio!(0x0400_0018 = BG2HOFS: VolAddress<u16, (), Safe>; "Background 2 Horizontal Offset (text mode)");
+def_mmio!(0x0400_001A = BG2VOFS: VolAddress<u16, (), Safe>; "Background 2 Vertical Offset (text mode)");
+def_mmio!(0x0400_001C = BG3HOFS: VolAddress<u16, (), Safe>; "Background 3 Horizontal Offset (text mode)");
+def_mmio!(0x0400_001E = BG3VOFS: VolAddress<u16, (), Safe>; "Background 3 Vertical Offset (text mode)");
+def_mmio!(0x0400_0020 = BG2PA: VolAddress<i16, (), Safe>; "Background 2 Param A (affine mode)");
+def_mmio!(0x0400_0022 = BG2PB: VolAddress<i16, (), Safe>; "Background 2 Param B (affine mode)");
+def_mmio!(0x0400_0024 = BG2PC: VolAddress<i16, (), Safe>; "Background 2 Param C (affine mode)");
+def_mmio!(0x0400_0026 = BG2PD: VolAddress<i16, (), Safe>; "Background 2 Param D (affine mode)");
+def_mmio!(0x0400_0028 = BG2X: VolAddress<i32, (), Safe>; "Background 2 X Reference Point, 8-bits fractional (affine/bitmap modes)");
+def_mmio!(0x0400_002C = BG2Y: VolAddress<i32, (), Safe>; "Background 2 Y Reference Point, 8-bits fractional (affine/bitmap modes)");
+def_mmio!(0x0400_0030 = BG3PA: VolAddress<i16, (), Safe>; "Background 3 Param A (affine mode)");
+def_mmio!(0x0400_0032 = BG3PB: VolAddress<i16, (), Safe>; "Background 3 Param B (affine mode)");
+def_mmio!(0x0400_0034 = BG3PC: VolAddress<i16, (), Safe>; "Background 3 Param C (affine mode)");
+def_mmio!(0x0400_0036 = BG3PD: VolAddress<i16, (), Safe>; "Background 3 Param D (affine mode)");
+def_mmio!(0x0400_0038 = BG3X: VolAddress<i32, (), Safe>; "Background 3 X Reference Point, 8-bits fractional (affine/bitmap modes)");
+def_mmio!(0x0400_003C = BG3Y: VolAddress<i32, (), Safe>; "Background 3 Y Reference Point, 8-bits fractional (affine/bitmap modes)");
+def_mmio!(0x0400_0040 = WIN0H: VolAddress<u8x2, (), Safe>; "Window 0 Horizontal: high=left, low=(right+1)");
+def_mmio!(0x0400_0042 = WIN1H: VolAddress<u8x2, (), Safe>; "Window 1 Horizontal: high=left, low=(right+1)");
+def_mmio!(0x0400_0044 = WIN0V: VolAddress<u8x2, (), Safe>; "Window 0 Vertical: high=top, low=(bottom+1)");
+def_mmio!(0x0400_0046 = WIN1V: VolAddress<u8x2, (), Safe>; "Window 1 Vertical: high=top, low=(bottom+1)");
+def_mmio!(0x0400_0048 = WININ: VolAddress<WindowInside, Safe, Safe>; "Controls the inside Windows 0 and 1");
+def_mmio!(0x0400_004A = WINOUT: VolAddress<WindowOutside, Safe, Safe>; "Controls inside the object window and outside of windows");
+def_mmio!(0x0400_004C = MOSAIC: VolAddress<Mosaic, (), Safe>; "Sets the intensity of all mosaic effects");
+def_mmio!(0x0400_0050 = BLDCNT: VolAddress<BlendControl, Safe, Safe>; "Sets color blend effects");
+def_mmio!(0x0400_0052 = BLDALPHA: VolAddress<u8x2, Safe, Safe>;"Sets EVA(low) and EVB(high) alpha blend coefficients, allows `0..=16`, in 1/16th units");
+def_mmio!(0x0400_0054 = BLDY: VolAddress<u16, (), Safe>;"Sets EVY brightness blend coefficient, allows `0..=16`, in 1/16th units");
+
+// Sound
 
 def_mmio!(0x0400_0060 = SOUND1CNT_L: VolAddress<u16, Safe, Safe>);
 def_mmio!(0x0400_0062 = SOUND1CNT_H: VolAddress<u16, Safe, Safe>);
@@ -79,6 +84,8 @@ def_mmio!(0x0400_0090 = WAVE_RAM: VolBlock<u16, Safe, Safe, 8>);
 def_mmio!(0x0400_00A0 = FIFO_A: VolAddress<u32, Safe, ()>);
 def_mmio!(0x0400_00A4 = FIFO_B: VolAddress<u32, Safe, ()>);
 
+// DMA
+
 def_mmio!(0x0400_00B0 = DMA0SAD: VolAddress<*const c_void, (), Unsafe>);
 def_mmio!(0x0400_00B4 = DMA0DAD: VolAddress<*mut c_void, (), Unsafe>);
 def_mmio!(0x0400_00B8 = DMA0CNT_L: VolAddress<u16, (), Unsafe>);
@@ -96,6 +103,8 @@ def_mmio!(0x0400_00D8 = DMA3DAD: VolAddress<*mut c_void, (), Unsafe>);
 def_mmio!(0x0400_00DC = DMA3CNT_L: VolAddress<u16, (), Unsafe>);
 def_mmio!(0x0400_00DE = DMA3CNT_H: VolAddress<u16, Safe, Unsafe>);
 
+// Timers
+
 def_mmio!(0x0400_0100 = TM0CNT_L: VolAddress<u16, Safe, Safe>);
 def_mmio!(0x0400_0102 = TM0CNT_H: VolAddress<u16, Safe, Safe>);
 def_mmio!(0x0400_0104 = TM1CNT_L: VolAddress<u16, Safe, Safe>);
@@ -104,6 +113,8 @@ def_mmio!(0x0400_0108 = TM2CNT_L: VolAddress<u16, Safe, Safe>);
 def_mmio!(0x0400_010A = TM2CNT_H: VolAddress<u16, Safe, Safe>);
 def_mmio!(0x0400_010C = TM3CNT_L: VolAddress<u16, Safe, Safe>);
 def_mmio!(0x0400_010E = TM3CNT_H: VolAddress<u16, Safe, Safe>);
+
+// Serial (part 1)
 
 def_mmio!(0x0400_0120 = SIODATA32: VolAddress<u32, Safe, Safe>);
 def_mmio!(0x0400_0120 = SIOMULTI0: VolAddress<u16, Safe, Safe>);
@@ -114,8 +125,12 @@ def_mmio!(0x0400_0128 = SIOCNT: VolAddress<u16, Safe, Safe>);
 def_mmio!(0x0400_012A = SIOMLT_SEND: VolAddress<u16, Safe, Safe>);
 def_mmio!(0x0400_012A = SIODATA8: VolAddress<u8, Safe, Safe>);
 
+// Keys
+
 def_mmio!(0x0400_0130 = KEYINPUT: VolAddress<u16, Safe, ()>);
 def_mmio!(0x0400_0132 = KEYCNT: VolAddress<u16, Safe, Safe>);
+
+// Serial (part 2)
 
 def_mmio!(0x0400_0134 = RCNT: VolAddress<u16, Safe, Safe>);
 def_mmio!(0x0400_0140 = JOYCNT: VolAddress<u16, Safe, Safe>);
@@ -123,13 +138,23 @@ def_mmio!(0x0400_0150 = JOY_RECV: VolAddress<u32, Safe, Safe>);
 def_mmio!(0x0400_0154 = JOY_TRANS: VolAddress<u32, Safe, Safe>);
 def_mmio!(0x0400_0158 = JOYSTAT: VolAddress<u8, Safe, Safe>);
 
+// Interrupts
+
 def_mmio!(0x0400_0200 = IE: VolAddress<IrqBits, Safe, Safe>);
 def_mmio!(0x0400_0202 = IF: VolAddress<IrqBits, Safe, Unsafe>);
 def_mmio!(0x0400_0204 = WAITCNT: VolAddress<u16, Safe, Unsafe>);
 def_mmio!(0x0400_0208 = IME: VolAddress<bool, Safe, Safe>);
 
+// Palette RAM (PALRAM)
+
 def_mmio!(0x0500_0000 = BACKDROP_COLOR: VolAddress<Color, Safe, Safe>; "Color that's shown when no BG or OBJ draws to a pixel");
 def_mmio!(0x0500_0000 = BG_PALETTE: VolBlock<Color, Safe, Safe, 256>);
 def_mmio!(0x0500_2000 = OBJ_PALETTE: VolBlock<Color, Safe, Safe, 256>);
 
+// Video RAM (VRAM)
+
 def_mmio!(0x0600_0000 = MODE3_BITMAP: VolBlock<Color, Safe, Safe, {240 * 160}>);
+
+// Object Attribute Memory (OAM)
+
+// TODO
--- src/video.rs
+++ src/video.rs
@@ -34,7 +34,7 @@ impl DisplayControl {
   u16_bool_field!(12, show_obj);
   u16_bool_field!(13, enable_win0);
   u16_bool_field!(14, enable_win1);
-  u16_bool_field!(15, enable_win_obj);
+  u16_bool_field!(15, enable_obj_win);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -54,3 +54,91 @@ impl DisplayStatus {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
 pub struct BackgroundControl(u16);
+impl BackgroundControl {
+  pub_const_fn_new_zeroed!();
+  u16_int_field!(0 - 1, priority);
+  u16_int_field!(2 - 3, charblock);
+  u16_bool_field!(6, is_mosaic);
+  u16_bool_field!(7, is_8bpp);
+  u16_int_field!(8 - 12, screenblock);
+  u16_bool_field!(13, is_affine_wrapping);
+  u16_int_field!(14 - 15, size);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct WindowInside(u16);
+impl WindowInside {
+  pub_const_fn_new_zeroed!();
+  u16_bool_field!(0, win0_bg0);
+  u16_bool_field!(1, win0_bg1);
+  u16_bool_field!(2, win0_bg2);
+  u16_bool_field!(3, win0_bg3);
+  u16_bool_field!(4, win0_obj);
+  u16_bool_field!(5, win0_effect);
+  u16_bool_field!(8, win1_bg0);
+  u16_bool_field!(9, win1_bg1);
+  u16_bool_field!(10, win1_bg2);
+  u16_bool_field!(11, win1_bg3);
+  u16_bool_field!(12, win1_obj);
+  u16_bool_field!(13, win1_effect);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct WindowOutside(u16);
+impl WindowOutside {
+  pub_const_fn_new_zeroed!();
+  u16_bool_field!(0, outside_bg0);
+  u16_bool_field!(1, outside_bg1);
+  u16_bool_field!(2, outside_bg2);
+  u16_bool_field!(3, outside_bg3);
+  u16_bool_field!(4, outside_obj);
+  u16_bool_field!(5, outside_effect);
+  u16_bool_field!(8, obj_win_bg0);
+  u16_bool_field!(9, obj_win_bg1);
+  u16_bool_field!(10, obj_win_bg2);
+  u16_bool_field!(11, obj_win_bg3);
+  u16_bool_field!(12, obj_win_obj);
+  u16_bool_field!(13, obj_win_effect);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct Mosaic(u16);
+impl Mosaic {
+  pub_const_fn_new_zeroed!();
+  u16_int_field!(0 - 3, bg_h_extra);
+  u16_int_field!(4 - 7, bg_v_extra);
+  u16_int_field!(8 - 11, obj_h_extra);
+  u16_int_field!(12 - 15, obj_v_extra);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(u16)]
+pub enum ColorEffectMode {
+  NoEffect = 0 << 6,
+  AlphaBlend = 1 << 6,
+  Brighten = 2 << 6,
+  Darken = 3 << 6,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct BlendControl(u16);
+impl BlendControl {
+  pub_const_fn_new_zeroed!();
+  u16_bool_field!(0, target1_bg0);
+  u16_bool_field!(1, target1_bg1);
+  u16_bool_field!(2, target1_bg2);
+  u16_bool_field!(3, target1_bg3);
+  u16_bool_field!(4, target1_obj);
+  u16_bool_field!(5, target1_backdrop);
+  u16_enum_field!(6 - 7: ColorEffectMode, mode);
+  u16_bool_field!(8, target2_bg0);
+  u16_bool_field!(9, target2_bg1);
+  u16_bool_field!(10, target2_bg2);
+  u16_bool_field!(11, target2_bg3);
+  u16_bool_field!(12, target2_obj);
+  u16_bool_field!(13, target2_backdrop);
+}
