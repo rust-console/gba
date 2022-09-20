b460a90c17c2c31ee4e06740338c811f9f770412
Lokathor <zefria@gmail.com>
2022-09-20 13:28:34 -0600

    single bit constants will be extremely useful

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -19,4 +19,28 @@ impl IrqBits {
   u16_bool_field!(11, dma3, with_dma3);
   u16_bool_field!(12, keypad, with_keypad);
   u16_bool_field!(13, gamepak, with_gamepak);
+
+  pub const VBLANK: Self = Self::new().with_vblank(true);
+  pub const HBLANK: Self = Self::new().with_hblank(true);
+  pub const VCOUNTER: Self = Self::new().with_vcounter(true);
+  pub const TIMER0: Self = Self::new().with_timer0(true);
+  pub const TIMER1: Self = Self::new().with_timer1(true);
+  pub const TIMER2: Self = Self::new().with_timer2(true);
+  pub const TIMER3: Self = Self::new().with_timer3(true);
+  pub const SERIAL: Self = Self::new().with_serial(true);
+  pub const DMA0: Self = Self::new().with_dma0(true);
+  pub const DMA1: Self = Self::new().with_dma1(true);
+  pub const DMA2: Self = Self::new().with_dma2(true);
+  pub const DMA3: Self = Self::new().with_dma3(true);
+  pub const KEYPAD: Self = Self::new().with_keypad(true);
+  pub const GAMEPAK: Self = Self::new().with_gamepak(true);
+
+  #[inline]
+  #[must_use]
+  pub const fn to_u16(self) -> u16 {
+    self.0
+  }
 }
+
+// TODO: might want to support bit ops. But it's not super important right now
+// since they can't be implented as const traits yet anyway.
