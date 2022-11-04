d412ee777941a00ef401200c83bd5c6c5b5696c2
Lokathor <zefria@gmail.com>
2022-11-04 16:17:49 -0600

    instruction inline example.

--- dump.sh
+++ dump.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cargo build --examples
 arm-none-eabi-objdump --headers --disassemble --demangle --architecture=armv4t --no-show-raw-insn -Mreg-names-std target/thumbv4t-none-eabi/debug/examples/hello >target/dump-hello.txt
+arm-none-eabi-objdump --headers --disassemble --demangle --architecture=armv4t --no-show-raw-insn -Mreg-names-std target/thumbv4t-none-eabi/debug/examples/instruction_inline >target/dump-instruction_inline.txt
---
+++ examples/instruction_inline.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+#![feature(isa_attribute)]
+
+use gba::prelude::*;
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+  loop {}
+}
+
+#[instruction_set(arm::a32)]
+fn make_3rd_bg_pal_entry_black() {
+  BG_PALETTE.index(3).write(Color::new());
+}
+
+#[no_mangle]
+extern "C" fn main() -> ! {
+  make_3rd_bg_pal_entry_black();
+  loop {}
+}
