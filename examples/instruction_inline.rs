5de0f5e9f2f50a3e3864742d4be595200332b546
Lokathor <zefria@gmail.com>
2022-12-01 19:13:31 -0700

    cleanup, make MIR inlining work in dev

--- examples/instruction_inline.rs
+++ examples/instruction_inline.rs
@@ -8,12 +8,16 @@ fn panic_handler(_: &core::panic::PanicInfo) -> ! {
   loop {}
 }
 
+//#[inline(always)]
 #[instruction_set(arm::a32)]
 fn make_3rd_bg_pal_entry_black() {
-  BG_PALETTE.index(3).write(Color::new());
+  let x: u16;
+  unsafe { core::arch::asm!("movs {}, #0", out(reg) x) };
+  BG_PALETTE.index(3).write(Color(x));
 }
 
 #[no_mangle]
+#[instruction_set(arm::t32)]
 extern "C" fn main() -> ! {
   make_3rd_bg_pal_entry_black();
   loop {}
