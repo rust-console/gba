488fa1ed886a1e4340bcdb3bb2f42be160146fa3
EvanTheProgrammer <86786575+EvanTheProgrammer@users.noreply.github.com>
2023-11-14 20:26:10 -0500

    Create mode3_realtime_example.r

---
+++ examples/mode3_realtime_example.r
@@ -0,0 +1,33 @@
+/* 
+* Made by Evan Goemer
+* Licenced under CC0
+*/
+
+#![no_std]
+#![no_main]
+
+use gba::prelude::*;
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+  loop {}
+}
+
+#[no_mangle]
+fn main() {
+    DISPCNT.write(
+        DisplayControl::new().with_video_mode(VideoMode::_3).with_show_bg2(true),
+    );
+
+    let red = 0;
+    let green = 255;
+    let blue = 0;
+
+    let color = Color::from_rgb(red, green, blue);
+
+    for y in 0..160 {
+        for x in 0..240 {
+            VIDEO3_VRAM.index(x, y).write(color);
+        }
+    }
+}
