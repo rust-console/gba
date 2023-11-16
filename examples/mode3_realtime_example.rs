e928c4350dbeca759baa792b4f330f7aa223f28a
Lokathor <zefria@gmail.com>
2023-11-16 10:34:40 -0700

    show different colors in each pixel.

--- examples/mode3_realtime_example.rs
+++ examples/mode3_realtime_example.rs
@@ -14,20 +14,24 @@ fn panic_handler(_: &core::panic::PanicInfo) -> ! {
 }
 
 #[no_mangle]
-fn main() {
+fn main() -> ! {
   DISPCNT.write(
     DisplayControl::new().with_video_mode(VideoMode::_3).with_show_bg2(true),
   );
 
-  let red = 0;
-  let green = 255;
-  let blue = 0;
-
-  let color = Color::from_rgb(red, green, blue);
+  let mut red = 0;
+  let mut green = 255;
+  let mut blue = 0;
 
   for y in 0..160 {
     for x in 0..240 {
+      let color = Color::from_rgb(red, green, blue);
       VIDEO3_VRAM.index(x, y).write(color);
+
+      red = (red + 1) % 256;
+      green = (green + 3) % 256;
+      blue = (blue + 5) % 256;
     }
   }
+  loop {}
 }
