5269101205c2bce6fdf241d7fb0e3bd4376a8ba6
Lokathor <zefria@gmail.com>
2024-09-16 14:50:04 -0600

    update the objdump script.

--- dump.bat
+++ dump.bat
@@ -1,3 +1,2 @@
-cargo build --examples --verbose
-arm-none-eabi-objdump --headers --disassemble --demangle --architecture=armv4t --no-show-raw-insn -Mreg-names-std target/thumbv4t-none-eabi/debug/examples/hello >target/dump-hello.txt
-arm-none-eabi-objdump --headers --disassemble --demangle --architecture=armv4t --no-show-raw-insn -Mreg-names-std target/thumbv4t-none-eabi/debug/examples/asm_scratch >target/dump-asm_scratch.txt
+cargo build --examples
+arm-none-eabi-objdump --headers --disassemble --demangle --architecture=armv4t --no-show-raw-insn -Mreg-names-std --visualize-jumps target/thumbv4t-none-eabi/debug/examples/mode3_pong_example_game >target/dump-mode3_pong_example_game.txt
